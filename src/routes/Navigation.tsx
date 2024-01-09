bda25e2e5c935de67a8c297d73cefd175b6ab3d6
Diomar Villarroel <diomarfvr@gmail.com>
2024-01-09 02:13:58 -0400

    lazyload basico listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,40 +1,48 @@
+import { Suspense } from 'react'
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom'
 import logo from '../assets/react.svg'
-import {LazyPage1, LazyPage2, LazyPage3} from '../01-lazyload/pages'
+import { routes } from './routes'
+
+
+// Nota: El componente Suspense te permite mostrar una interfaz alternativa o fallback hasta que sus hijos hayan terminado de cargar.
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
+}
+
 
 const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className='main-layout'>
-        <nav>
-          <img src={logo} alt='react-logo' style={{paddingTop: '1rem'}} />
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <div className='main-layout'>
+          <nav>
+            <img src={logo} alt='react-logo' style={{paddingTop: '1rem'}} />
 
-          <ul>
-            <li>
-              <NavLink to='/lazy1' className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy Page 1</NavLink>
-            </li>
+            <ul>
+              { /* TODO: Crear un navlink dinámicos */}
+              {
+                routes.map((ruta, index) => (
+                  <li key={`key-${index}-${ruta.to}`}>
+                    <NavLink to={ruta.to} className={ ({ isActive }) => isActive ? 'nav-active' : '' }>{ruta.name}</NavLink>
+                  </li>
+                ))
+              }
+            </ul>
 
-            <li>
-              <NavLink to='/lazy2' className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy Page 2</NavLink>
-            </li>
+          </nav>
 
-            <li>
-              <NavLink to='/lazy3' className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy Page 3</NavLink>
-            </li>
-          </ul>
+          <Routes>
+            {
+              routes.map((ruta, index) => (
+                <Route key={`key-${index}-${ruta.to}`} path={ruta.path} element={<ruta.Component />} />
+              ))
+            }
+            <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
 
-        </nav>
-
-        <Routes>
-          <Route path='lazy1' element={<LazyPage1 />} />
-          <Route path='lazy2' element={<LazyPage2 />} />
-          <Route path='lazy3' element={<LazyPage3 />} />
-
-          <Route path='/*' element={<Navigate to='/lazy1' replace />} />
-        </Routes>
-
-      </div>
-    </ BrowserRouter>
+        </div>
+      </ BrowserRouter>
+    </Suspense>
   )
 }
 
